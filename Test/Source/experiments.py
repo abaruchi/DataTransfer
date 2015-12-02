7bca99a24400c60b8434c741d08bf4f1bc8db865
Artur Baruchi <artur.baruchi@gmail.com>
2015-12-02 14:48:30 -0200

    Updated Test Class and Documentation.

--- Test/Source/experiments.py
+++ Test/Source/experiments.py
@@ -28,9 +28,9 @@ self.__contrato = contrato
 class Teste(object):
     __metaclass__ = ABCMeta
 
-    def __init__(self,duracao=60, myid='my_execution'):
+    def __init__(self, duracao=60, clientes=1):
         self.__duracao = duracao
-        self.__myid = myid
+        self.__clientes = clientes
         self.__timestamp = int(time.time())
 
     @property
@@ -38,61 +38,60 @@ class Teste(object):
         return self.__duracao
 
     @property
-    def myid(self):
-        return self.__myid
+    def clientes(self):
+        return self.__clientes
 
     @property
     def timestamp(self):
         return self.__timestamp
 
     @abstractmethod
-    def inicia_teste(self):
+    def inicia_server(self):
+        pass
+
+    @abstractmethod
+    def inicia_clientes(self):
         pass
 
     @abstractmethod
     def finaliza_teste(self):
-        pass
-
-    @abstractmethod
-    def armazena_resultado(self):
         pass
 
 class Mem2Mem(Teste):
 
-    def inicia_teste(self):
+    def inicia_clientes(self):
         print "Starting Test"
+
+    def inicia_server(self):
+        print "Finishing Test"
 
     def finaliza_teste(self):
         print "Finishing Test"
 
-    def armazena_resultado(self):
-        print "Logging Partial Results"
-
 class Disk2Disk(Teste):
 
-    def __init__(self, origem, destino, tamanho=5):
-        self.origem = origem
-        self.destino = destino
-        self.tamanho = tamanho
+    def __init__(self, arquivo_origem, arquivo_destino, tamanho=5):
+        self.arquivo_origem = arquivo_origem
+        self.arquivo_destino = arquivo_destino
+        self.arquivo_tamanho = tamanho
 
     def cria_arquivo (self):
         print "Creating File of %s Gb" % self.tamanho
 
-    def inicia_teste(self):
+    def inicia_clientes(self):
         print "Starting Test"
+
+    def inicia_server(self):
+        print "Finishing Test"
 
     def finaliza_teste(self):
         print "Finishing Test"
 
-    def armazena_resultado(self):
-        print "Logging Partial Results"
-
 
 class Log (object):
 
-    def __init__(self, arquivo, data, timestap):
+    def __init__(self, arquivo, timestap):
         self.__arquivo = arquivo
-        self.__data = data
         self.__timestamp = timestap
 
     def inicia_log(self):
